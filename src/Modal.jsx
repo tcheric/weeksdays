60c51f387674ede57eb5d202e4b22c3ed91ff9d1
tcheric <echen4401@gmail.com>
2024-06-08 19:15:37 +1000

    modal input form

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -4,10 +4,31 @@ const Modal = ({ open, onClose }) => {
 
   if (!open) return null
 
+  const [dob, setDob] = useState("")
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (!dob) {
+      alert('Please enter a date')
+      return
+    }
+    onAdd({ dob }) //this is an addTask object
+    setDob('')
+  }
+
   return (
     <>
       <div className="modal-bg" onClick={onClose}>
         <div className="popup" onClick={(e)=> {e.stopPropagation()}}>
+          <form className="dob-form" onSubmit={onSubmit}>
+            <label className="label">Enter DOB:</label>
+            <input 
+              className={"input"}
+              type='date' 
+              value = {dob} 
+              onChange={(e) => setName(e.target.value)}
+            />
+          </form>
         </div>
       </div>
     </>
