14b38c95dc913da9b40f4fbd9d1bd89200b5f4b3
tcheric <echen4401@gmail.com>
2024-06-27 00:05:29 +1000

    formatting and week page logic

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react'
+import { FaDotCircle } from "react-icons/fa";
+import { GoDash, GoCircle } from "react-icons/go";
 
-const Goal = ({  }) => {
-
+const Goal = ({ name }) => {
+  // 1 = dot, 0 = dash
+  const dotArr = [1,1,1,0,1,1,1]
   return (
-  <>
-  <div>Hi</div>
-  </>
+  <div className="goal">
+    <span className="goal-name">name</span>
+    <div className="dots-lines">
+        {dotArr.map(i => {
+          if (i == 1) return <GoCircle className="dot" key={crypto.randomUUID()}/>
+          return <GoDash className="dash" key={crypto.randomUUID()}/>
+        })}
+    </div>
+  </div>
   )
 }
 
--- src/components/WeekPage.jsx
+++ src/components/WeekPage.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
+import Goal from "./Goal"
 
 const WeekPage = ({}) => {
-  const [goals, setgoals] = useState([])
+  // Hardcoded data
+  const days = ["M","T","W","T","F","S","S"]
+
+  const [goals, setgoals] = useState(["Knitting"])
 
   const params = useParams();
   
@@ -10,10 +14,14 @@ const WeekPage = ({}) => {
   <div className="wk-ctnr">
     <h1>W{params.weekNum}</h1> 
     <div className="outer-goal-ctnr">
-      <div className="day-axis"></div>
+      <div className="day-axis">
+        {days.map(i => {
+          return <span key={crypto.randomUUID()}>{i}</span>
+        })}
+      </div>
       <div className="goal-ctnr">
         {goals.map(i => {
-          return <Goal></Goal>
+          return <Goal name={i} key={crypto.randomUUID()}></Goal>
         })}
       </div>
     </div>
