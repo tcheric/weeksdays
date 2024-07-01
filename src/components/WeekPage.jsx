d301fe3752928905c15eac911bd3a2f99debaa83
tcheric <echen4401@gmail.com>
2024-07-01 23:25:49 +1000

    add goal button

--- src/components/WeekPage.jsx
+++ src/components/WeekPage.jsx
@@ -7,10 +7,16 @@ const WeekPage = ({}) => {
   const days = ["M","T","W","T","F","S","S"]
 
   const [goals, setgoals] = useState(["Kubernetes", "Leetcode"])
+  const [showInput, setShowInput] = useState(false)
+  const [newGoal, setNewGoal] = useState("")
 
   const params = useParams()
   const navigate = useNavigate();
-  
+
+  const addGoal = () => {
+    // TODO
+  }
+
   return (
   <>
     <div className="wk-ctnr">
@@ -25,6 +31,25 @@ const WeekPage = ({}) => {
           {goals.map(i => {
             return <Goal name={i} key={crypto.randomUUID()}></Goal>
           })}
+          <div className={`ag-btn-ctnr ${showInput ? 'input-showing': ''}`}>
+            {!showInput && <button 
+              className="add-goal-button"
+              onClick={() => setShowInput(true)}>
+              +
+            </button>}
+            {showInput && <input 
+              className="ag-input"
+              type='text' 
+              placeholder={"Add Goal"}
+              value = {newGoal} 
+              onChange={(e) => setNewGoal(e.target.value)}
+            />}
+            {showInput && <button 
+              className="add-goal-button"
+              onClick={() => setShowInput(true)}>
+              +
+            </button>}
+          </div>
         </div>
       </div>
     </div>
