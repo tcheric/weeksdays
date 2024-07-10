9ab5e5c77568824704c61cf1715192f85d68cfa5
tcheric <echen4401@gmail.com>
2024-07-10 23:59:58 +1000

    hover

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from 'react'
 import { FaDotCircle } from "react-icons/fa";
 import { GoDash, GoSquareFill } from "react-icons/go";
+import { RxDotsVertical } from "react-icons/rx";
 
 const Goal = ({ name }) => {
   // 1 = dot, 0 = dash
   const dotArr = [1,1,1,0,1,1,1]
+
+  // const [showDots, setShowDots] = useState(false)
+  // useEffect(() => {
+  //   document.getElementById(name).addEventListener("mouseover", (event) => {setShowDots(!showDots)});
+  
+  //   return () => {
+  //   }
+  // }, [])
+  
+  
+
+
+
   return (
   <div className="goal">
-    <span className="goal-name">{name}</span>
+    <div className="goal-name" id={name} onClick={()=>{setShowDots(!showDots)}}>
+      <div className="inner-gn-wrapper">
+        {name}
+        {/* {name}{showDots && <RxDotsVertical className="goal-name-dots"/>} */}
+      </div>
+    </div>
     <div className="dots-lines">
         {dotArr.map(i => {
           if (i == 1) return <GoSquareFill className="dot" key={crypto.randomUUID()}/>
